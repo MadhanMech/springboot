01e2bfe3be795cbf01a8a480d3d4ddcd83a71bdb
YaaMadhan2000 <yournewemail@example.com>
2025-10-20 08:01:56 +0530

    sdf

--- ConditionOnProperty/src/main/java/com/example/ConditionOnProperty/ConditionOnPropertyApplication.java
+++ ConditionOnProperty/src/main/java/com/example/ConditionOnProperty/ConditionOnPropertyApplication.java
@@ -8,6 +8,7 @@ public class ConditionOnPropertyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConditionOnPropertyApplication.class, args);
+		System.out.println("dsfsd");
 	}
 
 }
