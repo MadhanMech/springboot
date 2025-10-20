45e263f153edf10b8cf2b097222a527215de45c0
YaaMadhan2000 <yournewemail@example.com>
2025-10-20 08:00:00 +0530

    ConditionOnProperty

---
+++ ConditionOnProperty/src/main/java/com/example/ConditionOnProperty/ConditionOnPropertyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConditionOnProperty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionOnPropertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionOnPropertyApplication.class, args);
+	}
+
+}
---
+++ ConditionOnProperty/src/test/java/com/example/ConditionOnProperty/ConditionOnPropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConditionOnProperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConditionOnPropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
